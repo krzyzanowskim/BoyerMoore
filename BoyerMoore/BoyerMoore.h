e3d6f2ccc2d5ba10bcbd63b4bd78ae8bf080bd43
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2015-08-15 10:26:11 +0200

    Initial Commit

---
+++ BoyerMoore/BoyerMoore.h
@@ -0,0 +1,19 @@
+//
+//  BoyerMoore.h
+//  BoyerMoore
+//
+//  Created by Marcin Krzyzanowski on 15/08/15.
+//  Copyright © 2015 Marcin Krzyżanowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BoyerMoore.
+FOUNDATION_EXPORT double BoyerMooreVersionNumber;
+
+//! Project version string for BoyerMoore.
+FOUNDATION_EXPORT const unsigned char BoyerMooreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoyerMoore/PublicHeader.h>
+
+
